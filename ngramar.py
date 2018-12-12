c76ae80e33e90666e6b6c887ac78de7a3380edab
Sebastião Barata <sebastiao.barata@gmail.com>
2018-12-12 23:25:05 +0000

    Implementing code

--- ngramar.py
+++ ngramar.py
@@ -1,10 +1,24 @@
 import sys
+import codecs
+import re
 
-# get the string
+# get the string / readfile
+
+contents = ""
+with codecs.open('teste.txt', 'r', 'utf-8') as file_object:
+    contents = file_object.read()
+
+pattern = re.compile('([^\s\w]|_)+')
+clean_contents = pattern.sub('', contents)
+clean_contents = re.sub("\d+", "", clean_contents)
+clean_contents = re.sub("[\t ]+", " ", clean_contents)
+clean_contents = re.sub("\n+", "\n", clean_contents)
+
+print(clean_contents)
+
+# Clean the file (get rid of the pontuations and numbers, and of double spasing )
 
 # create the file rollers
-
-# avoid pontuations and numbers
 
 # Doubt should I normalize the string before o during rolling
 
@@ -12,6 +26,6 @@ import sys
 
 # roll over the string feeding the hash table with histagram
 
-# order histagram (top 300)
+
 
 # return vector
